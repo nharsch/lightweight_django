f3aab60e6d62fd1e552d3d0701823765855504ce
nharsch <nharsch@nigels-mbp.vcnyc.indemand.net>
2015-11-05 12:06:23 -0500

    ch01 hello world script

---
+++ hello.py
@@ -0,0 +1,30 @@
+import sys
+
+from django.conf import settings
+
+# Need to set up these settings before making any additional imports from Django
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='thisisthesecretkey',
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+from django.conf.urls import url
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Hello World')
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+# if called with interpreter, run with command line options
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
